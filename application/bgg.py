0dec1c6dc594a2d4401bdcc25cc9b1054e3b448b
Mac Ryan <quasipedia@gmail.com>
2015-05-24 01:35:56 +0200

    DS - Debug strings to be tried online

--- application/bgg.py
+++ application/bgg.py
@@ -1,12 +1,21 @@
 import json
+import ConfigParser
+
 from boardgamegeek import BoardGameGeek
 from werkzeug.wrappers import Request, Response
 
-bgg = BoardGameGeek()
+
+def get_secrets():
+    config = ConfigParser.ConfigParser()
+    config.read('secrets.ini')
+    return [config.get('bgg', x) for x in ('incoming_token', 'hook_url')]
 
 
 @Request.application
 def application(request):
+    print request.data
+    bgg = BoardGameGeek()
+    incoming_token, hook_url = get_secrets()
     g = bgg.game("Jaipur")
     return Response(json.dumps(g.name))
 
