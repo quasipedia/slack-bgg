05a18e8811c61efb29978d0ce5d345c407175afc
Mac Ryan <quasipedia@gmail.com>
2015-05-24 03:00:24 +0200

    Basic functionality implemented!

--- application/bgg.py
+++ application/bgg.py
@@ -11,18 +11,35 @@ def get_secrets():
     config.read('secrets.ini')
     return [config.get('bgg', x) for x in ('incoming_token', 'hook_url')]
 
-valid_token, hook_url = get_secrets()
+VALID_TOKEN, HOOK_URL = get_secrets()
+BGG = BoardGameGeek()
+HELP_TEXT = 'Help text.'
+
+
+def process(query):
+    '''Process a valid query.'''
+    # Perform a global search
+    hits = BGG.search(query, 4 | 8)  # 4 | 8 == games + expansions
+    # Try to match the title exactly
+    if len(hits) == 1:
+        game = BGG.game(game_id=hits[0].id)
+        return Response('[{}] - {}'.format(game.year, game.name))
+    d = {'boardgame': 'G', 'boardgameexpansion': 'E'}
+    data = [u'{} ({}) - {} - #{}'.format(d[h.type], h.year, h.name, h.id)
+            for h in hits]
+    return Response(u'\n'.join(sorted(data, reverse=True)))
 
 
 @Request.application
 def application(request):
     if request.method != 'POST':
         abort(405)
-    if request.form.get('token') != valid_token:
+    if request.form.get('token') != VALID_TOKEN:
         abort(401)
-    bgg = BoardGameGeek()
-    g = bgg.game("Jaipur")
-    return Response(json.dumps(g.name))
+    query = request.form.get('text')
+    if not query:
+        return Response(HELP_TEXT)
+    return process(query)
 
 
 if __name__ == '__main__':
