eab6953c258c0c3e3c55182b071ea9ae9ee90c95
Mac Ryan <quasipedia@gmail.com>
2015-05-24 15:55:33 +0200

    Tweak the list result display

--- application/bgg.py
+++ application/bgg.py
@@ -2,7 +2,7 @@ import json
 import requests
 import ConfigParser
 
-from boardgamegeek import BoardGameGeek
+from boardgamegeek import BoardGameGeek, BoardGameGeekAPIError
 from werkzeug.wrappers import Request, Response
 from werkzeug.exceptions import abort
 
@@ -18,7 +18,7 @@ SITE_URL = 'https://boardgamegeek.com'
 BOT_NAME = 'BoardGameGeek'
 ICON_URL = 'https://slack.com/img/icons/app-57.png'
 HELP_TEXT = open('help.md').read()
-MAX_LIST_LENGTH = 5
+MAX_LIST_LENGTH = 15
 
 
 def fire_hook(payload):
@@ -28,11 +28,17 @@ def fire_hook(payload):
 
 def display_game(channel, game_id):
     '''Retrieve and format a game based on its game ID.'''
+    try:
+        game = BGG.game(game_id=int(game_id[1:]))
+    except ValueError:
+        abort(400)  # Bad request
+    except BoardGameGeekAPIError:
+        abort(404)  # Not found
     payload = {
         'channel': '#{}'.format(channel),
         'username': BOT_NAME,
         'icon_url': ICON_URL,
-        'text': 'Single game',
+        'text': game.name,
         # 'attachments': cells
     }
     fire_hook(payload)
@@ -46,23 +52,31 @@ def search_games(user, query):
     cells = []
     for year, name, type_, id_ in data[:MAX_LIST_LENGTH]:
         url = '{}/{}/{}'.format(SITE_URL, type_, id_)
-        htype = 'Game' if type_ == 'boardgame' else 'Expansion'
-        color = 'good' if type_ == 'boardgame' else 'warning'
+        color = '#330099' if type_ == 'boardgame' else '#66CCFF'
         cells.append({
             'fallback': u'{} <{}>'.format(name, url),
             'color': color,
             'fields': [
                 {
                     'title': name,
-                    'value': u'{} ({}) <{}|{}>'.format(htype, year, url, id_),
+                    'value': u'({}) ID: #{} - <{}|BGG page>'.format(
+                        year, id_, url),
                 }
             ]
         })
+    hnum = len(hits)
+    if hnum == 0:
+        text = 'Sorry, no matches for your query. :('
+    else:
+        text = '{} matching items.'.format(hnum)
+        if hnum > MAX_LIST_LENGTH:
+            text = '{} Showing the first {} results.'.format(
+                text, MAX_LIST_LENGTH)
     payload = {
         'channel': '@{}'.format(user),
         'username': BOT_NAME,
         'icon_url': ICON_URL,
-        'text': '{} matching items'.format(len(hits)),
+        'text': text,
         'attachments': cells}
     fire_hook(payload)
 
@@ -70,9 +84,9 @@ def search_games(user, query):
 @Request.application
 def application(request):
     if request.method != 'POST':
-        abort(405)
+        abort(405)  # Method not allowed
     if request.form.get('token') != VALID_TOKEN:
-        abort(401)
+        abort(401)  # Unauthorized
     user_name = request.form['user_name']
     channel_name = request.form['channel_name']
     query = request.form.get('text')
--- application/help.md
+++ application/help.md
@@ -2,3 +2,4 @@ This command integrates <https://boardgamegeek.com|BoardGameGeek>.  Usage:
 `/bgg`: show this help text *[to user]*.
 `/bgg <search-terms>`: search games and expansions *[to user]*.
 `/bgg #<game-id-number>`: show detailed information on single game *[to channel]*.
+Search results are sorted by reverse year of publication, *dark blue* items are games, *light blue* are expansions.
