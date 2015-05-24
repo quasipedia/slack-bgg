b058c61d5409bde3132edc2437d1467ceeb88af2
Mac Ryan <quasipedia@gmail.com>
2015-05-24 04:28:16 +0200

    Hook step 1: List of games

--- application/bgg.py
+++ application/bgg.py
@@ -1,4 +1,5 @@
 import json
+import requests
 import ConfigParser
 
 from boardgamegeek import BoardGameGeek
@@ -13,21 +14,53 @@ def get_secrets():
 
 VALID_TOKEN, HOOK_URL = get_secrets()
 BGG = BoardGameGeek()
-HELP_TEXT = 'Help text.'
+SITE_URL = 'https://boardgamegeek.com'
+BOT_NAME = 'BoardGameGeek'
+ICON_URL = 'https://slack.com/img/icons/app-57.png'
+HELP_TEXT = 'Help text\nfoo\nbar.'
+MAX_LIST_LENGTH = 5
 
 
-def process(query):
+def send_game(channel, game):
+    '''Send data about a specifig game to Slack.'''
+
+
+def send_list(channel, hits):
+    '''Send data about a specifig game to Slack.'''
+    cells = []
+    for year, name, type_, id_ in hits[:MAX_LIST_LENGTH]:
+        url = '{}/{}/{}'.format(SITE_URL, type_, id_)
+        htype = 'Game' if type_ == 'boardgame' else 'Expansion'
+        color = 'good' if type_ == 'boardgame' else 'warning'
+        cells.append({
+            'fallback': u'{} <{}>'.format(name, url),
+            'color': color,
+            'fields': [
+                {
+                    'title': name,
+                    'value': u'{} ({}) <{}|{}>'.format(htype, year, url, id_),
+                }
+            ]
+        })
+    payload = {
+        'channel': '#{}'.format(channel),
+        'username': BOT_NAME,
+        'icon_url': ICON_URL,
+        'text': '{} matching items'.format(len(hits)),
+        'attachments': cells}
+    requests.post(HOOK_URL, data={'payload': json.dumps(payload)})
+
+
+def process(channel, query):
     '''Process a valid query.'''
     # Perform a global search
     hits = BGG.search(query, 4 | 8)  # 4 | 8 == games + expansions
     # Try to match the title exactly
     if len(hits) == 1:
-        game = BGG.game(game_id=hits[0].id)
-        return Response('[{}] - {}'.format(game.year, game.name))
-    d = {'boardgame': 'G', 'boardgameexpansion': 'E'}
-    data = [u'{} ({}) - {} - #{}'.format(d[h.type], h.year, h.name, h.id)
-            for h in hits]
-    return Response(u'\n'.join(sorted(data, reverse=True)))
+        send_game(channel, BGG.game(game_id=hits[0].id))
+    else:
+        hits = [(h.year, h.name, h.type, h.id) for h in hits]
+        send_list(channel, sorted(hits, reverse=True))
 
 
 @Request.application
@@ -36,10 +69,12 @@ def application(request):
         abort(405)
     if request.form.get('token') != VALID_TOKEN:
         abort(401)
+    channel = request.form['channel_name']
     query = request.form.get('text')
     if not query:
         return Response(HELP_TEXT)
-    return process(query)
+    process(channel, query)
+    return Response()
 
 
 if __name__ == '__main__':
